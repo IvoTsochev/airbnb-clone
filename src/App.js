c7d7330d2bd73a0f422f8009b6e76ca41a404af0
IvoTsochev <ivo.tsochev@live.com>
2020-10-31 18:08:57 +0200

    finiiiiiished

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
 import Footer from "./Footer";
+import SearchPage from "./SearchPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
-      {/* Home */}
-      {/* Header */}
+      <Router>
+        <Header />
 
-      {/* Banner */}
-      {/* Search */}
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
 
-      {/* Cards */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
 
-      {/* Footer */}
-
-      {/* SearchPage */}
-      {/* Header */}
+        <Footer />
+      </Router>
     </div>
   );
 }
