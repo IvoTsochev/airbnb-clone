308abbadb8cdd27b9de200828b4a455dda07f3c2
IvoTsochev <ivo.tsochev@live.com>
2020-10-31 10:50:46 +0200

    footer

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@material-ui/core";
+import Search from "./Search";
 
 function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div className="banner">
+      {showSearch && <Search />}
+
+      <div className="banner__search">
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          className="banner__searchButton"
+          variant="outlined"
+        >
+          {showSearch ? "Hide" : "Search Dates"}
+        </Button>
+      </div>
       <div className="banner__info">
         <h1>Get out and stretch your imagination</h1>
         <h5>
